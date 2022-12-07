711094b75b2519f9a3392fbcaac986a082d0b276
Reidelbach <mreidelb@dry.zib.de>
2022-12-07 10:54:25 +0100

    Initial Version

---
+++ func/export.py
@@ -0,0 +1,188 @@
+from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
+from django.conf import settings
+from rdmo.projects.exports import Export
+from rdmo.questions.models import Question
+from rdmo.core.utils import render_to_csv
+
+from .para import *
+import requests
+
+from owlready2 import *
+import nltk
+import numpy as np
+import sklearn as sk
+
+class MaRDIExport(Export):
+
+    def render(self):
+        '''Function that renders User answers to MaRDI template
+           (adjusted from csv export)'''
+       
+        # Check if MaRDI Questionaire is used
+        if str(self.project.catalog) != 'MaRDI':
+            return HttpResponse('Questionnaire not suitable for MaRDI Export!',content_type="text/plain") 
+
+        # Get Data - Questions and User Answers
+        queryset = self.project.values.filter(snapshot=None)
+        data = []
+        for question in Question.objects.order_by_catalog(self.project.catalog):
+            if question.questionset.is_collection and question.questionset.attribute:
+                if question.questionset.attribute.uri.endswith('/id'):
+                    set_attribute_uri = question.questionset.attribute
+                else:
+                    set_attribute_uri = question.questionset.attribute.uri.rstrip('/') + '/id'
+                for value_set in queryset.filter(attribute__uri=question.questionset.attribute.uri):
+                    values = queryset.filter(attribute=question.attribute, set_index=value_set.set_index) \
+                                     .order_by('set_prefix', 'set_index', 'collection_index')
+                    data.append([question.attribute.uri+'_'+self.stringify(value_set.value), self.stringify_values(values)])
+            else:
+                values = queryset.filter(attribute=question.attribute).order_by('set_prefix', 'set_index', 'collection_index')
+                data.append([question.attribute.uri, self.stringify_values(values)])
+        
+        # Workflow Documentation
+        if dec[0][0] in data or dec[0][1] in data:
+            # Export as Markdown File
+            if dec[2][0] in data: 
+                # Adjust raw MaRDI templates to User answers
+                temp=self.dyn_template(data) 
+                if len(temp) == 0:
+                    return HttpResponse('Choose a Workflow Type!',content_type="text/plain") 
+                # Fill out MaRDI template
+                for entry in data:
+                        temp=re.sub(entry[0],entry[1],temp)
+                # Download as Markdown
+                response = HttpResponse(temp, content_type="application/md")
+                response['Content-Disposition'] = 'filename="workflow.md"'
+                return response
+            
+            # Export to MaRDI Portal
+            elif dec[2][1] in data:
+                '''This is a placeholder for the MaRDI Portal functionality. Until ready, documented Workflows 
+                   in RDMO will be saved to a local owl ontology file.
+                   Documented workflows will be new instances in Workflow class and described by three datatype 
+                   properties hasMethod, hasInputData, and hasResearchObjective.'''
+                # Load Ontology
+                onto=get_ontology(settings.BASE_DIR+'/mardi_workflow/kg/MaRDI_RDMO.owl').load()
+                # Prepare new instance
+                new=onto.Workflow(self.project.title)
+                research_objective=''
+                input_data=''
+                method=''
+                for entry in data:
+                    if KG_export[0] in entry[0]:
+                        research_objective=research_objective+entry[1]+' ; '
+                    elif KG_export[1] in entry[0]:
+                        method=method+entry[1]+' ; '
+                    elif KG_export[2] in entry[0]:
+                        input_data=input_data+entry[1]+' ; '
+                new.hasResearchObjective=research_objective
+                new.hasMethod=method
+                new.hasInputData=input_data
+                onto.save()
+                return HttpResponse('Your Workflow has been added to the local Knowledge Graph!\n\nSee you soon on the MaRDI Portal!',content_type="text/plain")
+
+            # Not chosen
+            else:
+                return HttpResponse('Choose an Export Type',content_type="text/plain")
+
+        # Workflow Finding
+        elif dec[1][0] in data or dec[1][1] in data:
+            # Load Ontology
+            onto=get_ontology(settings.BASE_DIR+'/mardi_workflow/kg/MaRDI_RDMO.owl').load()
+            # What to search for?
+            if dec[3][0] in data or dec[3][1] in data:
+                verb='hasResearchObjective'
+            elif dec[4][0] in data or dec[4][1] in data:
+                verb='hasMethod'
+            elif dec[5][0] in data or dec[5][1] in data:
+                verb='hasInputData'
+            else:
+                return HttpResponse('Choose an entity to search for!',content_type="text/plain")
+            # SPARQL query to get workflows and objects of interest
+            workflows=list(default_world.sparql("""SELECT ?workflow ?searched_obj{ ?workflow MaRDI_RDMO:"""+verb+""" ?searched_obj . }"""))
+            # List of Objects of interest
+            obj=[]
+            for workflow in workflows:
+                obj.append(workflow[1])
+            # Use TFIDF to compare user entry with available data, preprocess both before comparison
+            sim_matrix = self.tfidf_similarity(self.preprocessing(obj)+self.preprocessing([data[2][1]]))
+            # Index of (most likely) interesting paper.
+            index=(sim_matrix-np.diag(np.diag(sim_matrix)))[-1].argmax(axis=0)
+            return HttpResponse('We found a workflow that might be interesting for you:\n\n'+str(workflows[index][0])+'\n\nPlease do not forget to document your own workflow at the end so that others can benefit from it as well.',content_type="text/plain")
+        
+        # Not chosen
+        else:
+            return HttpResponse('Choose an Operation Modus!',content_type="text/plain")
+
+    def stringify_values(self, values):
+        '''Original function from csv export'''
+        if values is not None:
+            return '; '.join([self.stringify(value.value_and_unit) for value in values])
+        else:
+            return ''
+
+    def stringify(self, el):
+        '''Original function from csv export'''
+        if el is None:
+            return ''
+        else:
+            return re.sub(r'\s+', ' ', str(el))
+
+    def create_table(self, column_topics, row_ids, rows):
+        '''Function that creates a markdwon table with headers.
+           Row number depends on user answers, dummy entries''' 
+        table=''
+        for row in range(rows):
+            table=table+'| '
+            for n,topic in enumerate(column_topics):
+                if row==0:
+                    table=table+topic+' | '
+                elif row==1:
+                    table=table+'-- | '
+                else:
+                    table=table+row_ids[n]+'_'+str(row-2)+' | '
+            table=table+'\n'
+        return table
+
+    def dyn_template(self, data):
+        '''Function that chooses proper raw MaRDI template and
+           inserts appropriate tables depending on user answers.'''
+        if dec[6][0] in data or dec[6][1] in data:
+            temp=math_temp
+            tables=math_tables
+            topics=math_topics
+            ids=math_ids
+        elif dec[7][0] in data or dec[7][1] in data:
+            temp=exp_temp
+            tables=exp_tables
+            topics=exp_topics
+            ids=exp_ids
+        else:
+            temp=[]
+            return temp
+        data_flat=sum(data,[])
+        temp=re.sub('TEMPLATE_TITLE',self.project.title,temp)
+        for n,table in enumerate(tables):
+            t=self.create_table(topics[n],ids[n],sum(ids[n][0] in s for s in data_flat)+2)
+            temp=re.sub(table,t,temp)
+        return(temp)
+
+    def preprocessing(self,ts):
+        '''This function does a simple preprocessing of strings'''
+        # Lower case, none letter removal
+        ts = [re.sub(' +',' ',re.sub('[^a-z ]+',' ',t.lower())) for t in ts]
+        # Stopword removal
+        ts = [[w for w in t.split() if w not in set(nltk.corpus.stopwords.words('english'))] for t in ts]
+        # Ending removal
+        ts = [" ".join([nltk.stem.PorterStemmer().stem(w) for w in t]) for t in ts]
+        return ts
+
+    def tfidf_similarity(self,documents):
+        '''This function takes a pool of documents, builds a tfidf model, 
+           and determines the similarity between all documents.'''
+        # Use TFIDF model
+        embeddings = sk.feature_extraction.text.TfidfVectorizer().fit_transform(documents)
+        # Calculate Cosine Similarities in array
+        cosine_similarities = np.reshape(sk.metrics.pairwise.cosine_similarity(embeddings, embeddings).flatten(),(len(documents),len(documents)))
+        return cosine_similarities
+       
---
+++ func/para.py
@@ -0,0 +1,280 @@
+# Raw Math Template
+
+math_temp='''# TEMPLATE_TITLE 
+
+PID (if applicable): http://example.com/terms/domain/MaRDI/Section_2/Set_1/Question_2
+
+## Problem Statement
+
+http://example.com/terms/domain/MaRDI/Section_2/Set_1/Question_1
+
+### Object of Research and Objective
+
+http://example.com/terms/domain/MaRDI/Section_2/Set_2/Question_1
+
+### Procedure
+
+http://example.com/terms/domain/MaRDI/Section_2/Set_2/Question_2
+
+### Involved Disciplines
+
+http://example.com/terms/domain/MaRDI/Section_2/Set_3/Question_1
+
+### Data Streams
+
+http://example.com/terms/domain/MaRDI/Section_2/Set_3/Question_2
+
+## Model
+
+http://example.com/terms/domain/MaRDI/Section_3/Set_1/Question_1
+
+### Discretization
+
+* Time: http://example.com/terms/domain/MaRDI/Section_3/Set_1/Question_2
+* Space: http://example.com/terms/domain/MaRDI/Section_3/Set_1/Question_3
+
+### Variables
+
+MATH_TAB_1 
+
+## Process Informationen
+
+### Process Steps
+
+MATH_TAB_2
+
+### Applied Methods 
+
+MATH_TAB_3
+
+### Software used
+
+MATH_TAB_4
+
+### Hardware
+
+MATH_TAB_5
+
+### Input Data
+
+MATH_TAB_6
+
+### Output Data
+
+MATH_TAB_7
+
+## Reproducibility
+
+### Mathematical Reproducibility 
+
+http://example.com/terms/domain/MaRDI/Section_5/Set_1/Question_1
+
+### Runtime Reproducibility 
+
+http://example.com/terms/domain/MaRDI/Section_5/Set_1/Question_2
+
+### Reproducibility of Results
+
+http://example.com/terms/domain/MaRDI/Section_5/Set_1/Question_3
+
+### Reproducibility on original Hardware
+
+http://example.com/terms/domain/MaRDI/Section_5/Set_1/Question_4
+
+### Reproducibility on other Hardware
+
+http://example.com/terms/domain/MaRDI/Section_5/Set_1/Question_5
+
+### Transferability to
+
+http://example.com/terms/domain/MaRDI/Section_5/Set_1/Question_6
+
+# Legend
+
+The following abbreviations are used in the document to indicate/resolve IDs:
+
+doi: DOI / https://dx.doi.org/
+
+sw: swMATH / https://swmath.org/software/
+
+wikidata: https://www.wikidata.org/wiki/'''
+
+# Raw Exp Template
+
+exp_temp='''# TEMPLATE_TITLE
+
+PID (if applicable): http://example.com/terms/domain/MaRDI/Section_2/Set_1/Question_2
+
+## Problem Statement
+
+http://example.com/terms/domain/MaRDI/Section_2/Set_1/Question_1
+
+### Object of Research and Objective
+
+http://example.com/terms/domain/MaRDI/Section_2/Set_2/Question_1
+
+### Procedure
+
+http://example.com/terms/domain/MaRDI/Section_2/Set_2/Question_2
+
+### Involved Disciplines
+
+http://example.com/terms/domain/MaRDI/Section_2/Set_3/Question_1
+
+### Data Streams
+
+http://example.com/terms/domain/MaRDI/Section_2/Set_3/Question_2
+
+## Model
+
+http://example.com/terms/domain/MaRDI/Section_3/Set_1/Question_1
+
+### Discretization
+(if applicable)
+
+* Time: http://example.com/terms/domain/MaRDI/Section_3/Set_1/Question_2
+* Space: http://example.com/terms/domain/MaRDI/Section_3/Set_1/Question_3
+
+### Variables
+
+EXP_TAB_1
+
+### Parameter
+
+EXP_TAB_2
+
+## Process Informationen
+
+### Process Steps
+
+EXP_TAB_3
+
+
+### Applied Methods 
+
+EXP_TAB_4
+
+### Software used
+
+EXP_TAB_5
+
+### Experimental Devices/Instruments and Computer-Hardware
+
+EXP_TAB_6
+
+### Input Data
+
+EXP_TAB_7
+
+### Output Data
+
+EXP_TAB_8
+
+## Reproducibility
+
+### Reproducibility of the Experiments on the original Devices/Instruments/Hardware
+
+http://example.com/terms/domain/MaRDI/Section_5/Set_2/Question_1
+
+### Reproducibility of the Experiments on other Devices/Instruments/Hardware
+
+http://example.com/terms/domain/MaRDI/Section_5/Set_2/Question_2
+
+### Transferability of the  Experiments to
+
+http://example.com/terms/domain/MaRDI/Section_5/Set_2/Question_3
+
+# Legend
+
+The following abbreviations are used in the document to indicate/resolve IDs:
+
+doi: DOI / https://dx.doi.org/
+
+sw: swMATH / https://swmath.org/software/
+
+wikidata: https://www.wikidata.org/wiki/'''
+
+# Stuff to generate Tables for Math Template
+
+math_tables=['MATH_TAB_1','MATH_TAB_2','MATH_TAB_3','MATH_TAB_4','MATH_TAB_5','MATH_TAB_6','MATH_TAB_7']
+
+math_topics=[['Name','Unit','Symbol'],
+             ['Name','Description','Input','Output','Method','Parameter','Environment','Mathematical Area'],
+             ['ID','Name','Process Step','Parameter','implemented by'],
+             ['ID','Name','Description','Version','Programming Language','Dependencies','versioned','published','documented'],
+             ['ID','Name','Processor','Compiler','#Nodes','#Cores'],
+             ['ID','Name','Size','Data Structure','Format Representation','Format Exchange','binary/text','proprietary','to publish','to archive'],
+             ['ID','Name','Size','Data Structure','Format Representation','Format Exchange','binary/text','proprietary','to publish','to archive']]
+
+math_ids=[['http://example.com/terms/domain/MaRDI/Section_3/Set_2/Question_1','http://example.com/terms/domain/MaRDI/Section_3/Set_2/Question_2','http://example.com/terms/domain/MaRDI/Section_3/Set_2/Question_3'],
+          ['http://example.com/terms/domain/MaRDI/Section_4/Set_1/Question_1','http://example.com/terms/domain/MaRDI/Section_4/Set_1/Question_2','http://example.com/terms/domain/MaRDI/Section_4/Set_1/Question_3',
+           'http://example.com/terms/domain/MaRDI/Section_4/Set_1/Question_4','http://example.com/terms/domain/MaRDI/Section_4/Set_1/Question_5','http://example.com/terms/domain/MaRDI/Section_4/Set_1/Question_6',
+           'http://example.com/terms/domain/MaRDI/Section_4/Set_1/Question_7','http://example.com/terms/domain/MaRDI/Section_4/Set_1/Question_8'],
+          ['http://example.com/terms/domain/MaRDI/Section_4/Set_2/Question_1','http://example.com/terms/domain/MaRDI/Section_4/Set_2/Question_2','http://example.com/terms/domain/MaRDI/Section_4/Set_2/Question_3',
+           'http://example.com/terms/domain/MaRDI/Section_4/Set_2/Question_4','http://example.com/terms/domain/MaRDI/Section_4/Set_2/Question_5'],
+          ['http://example.com/terms/domain/MaRDI/Section_4/Set_3/Question_1','http://example.com/terms/domain/MaRDI/Section_4/Set_3/Question_2','http://example.com/terms/domain/MaRDI/Section_4/Set_3/Question_3',
+           'http://example.com/terms/domain/MaRDI/Section_4/Set_3/Question_4','http://example.com/terms/domain/MaRDI/Section_4/Set_3/Question_5','http://example.com/terms/domain/MaRDI/Section_4/Set_3/Question_6',
+           'http://example.com/terms/domain/MaRDI/Section_4/Set_3/Question_7','http://example.com/terms/domain/MaRDI/Section_4/Set_3/Question_8','http://example.com/terms/domain/MaRDI/Section_4/Set_3/Question_9'],
+          ['http://example.com/terms/domain/MaRDI/Section_4/Set_4/Question_1','http://example.com/terms/domain/MaRDI/Section_4/Set_4/Question_2','http://example.com/terms/domain/MaRDI/Section_4/Set_4/Question_3',
+           'http://example.com/terms/domain/MaRDI/Section_4/Set_4/Question_4','http://example.com/terms/domain/MaRDI/Section_4/Set_4/Question_5','http://example.com/terms/domain/MaRDI/Section_4/Set_4/Question_5'],
+          ['http://example.com/terms/domain/MaRDI/Section_4/Set_6/Question_0','http://example.com/terms/domain/MaRDI/Section_4/Set_6/Question_1','http://example.com/terms/domain/MaRDI/Section_4/Set_6/Question_2',
+           'http://example.com/terms/domain/MaRDI/Section_4/Set_6/Question_3','http://example.com/terms/domain/MaRDI/Section_4/Set_6/Question_4','http://example.com/terms/domain/MaRDI/Section_4/Set_6/Question_5',
+           'http://example.com/terms/domain/MaRDI/Section_4/Set_6/Question_6','http://example.com/terms/domain/MaRDI/Section_4/Set_6/Question_7','http://example.com/terms/domain/MaRDI/Section_4/Set_6/Question_8',
+           'http://example.com/terms/domain/MaRDI/Section_4/Set_6/Question_9'],
+          ['http://example.com/terms/domain/MaRDI/Section_4/Set_7/Question_0','http://example.com/terms/domain/MaRDI/Section_4/Set_7/Question_1','http://example.com/terms/domain/MaRDI/Section_4/Set_7/Question_2',
+           'http://example.com/terms/domain/MaRDI/Section_4/Set_7/Question_3','http://example.com/terms/domain/MaRDI/Section_4/Set_7/Question_4','http://example.com/terms/domain/MaRDI/Section_4/Set_7/Question_5',
+           'http://example.com/terms/domain/MaRDI/Section_4/Set_7/Question_6','http://example.com/terms/domain/MaRDI/Section_4/Set_7/Question_7','http://example.com/terms/domain/MaRDI/Section_4/Set_7/Question_8',
+           'http://example.com/terms/domain/MaRDI/Section_4/Set_7/Question_9']]
+
+# Stuff to generate Tables for Esp Template
+
+exp_tables=['EXP_TAB_1','EXP_TAB_2','EXP_TAB_3','EXP_TAB_4','EXP_TAB_5','EXP_TAB_6','EXP_TAB_7','EXP_TAB_8']
+
+exp_topics=[['Name','Unit','Symbol','dependent (measured) / independent (controlled)'],
+            ['Name','Unit','Symbol'],
+            ['Name','Description','Input','Output','Method','Parameter','Environment','Mathematical Area'],
+            ['ID','Name','Process Step','Parameter','realised / implemented by'],
+            ['ID','Name','Description','Version','Programming Language','Dependencies','versioned','published','documented'],
+            ['ID','Name','Description','Version','Part Nr','Serial Nr','Location','Software'],
+            ['ID','Name','Size','Data Structure','Format Representation','Format Exchange','binary/text','proprietary','to publish','to archive'],
+            ['ID','Name','Size','Data Structure','Format Representation','Format Exchange','binary/text','proprietary','to publish','to archive']]
+
+exp_ids=[['http://example.com/terms/domain/MaRDI/Section_3/Set_2/Question_1','http://example.com/terms/domain/MaRDI/Section_3/Set_2/Question_2','http://example.com/terms/domain/MaRDI/Section_3/Set_2/Question_3',
+          'http://example.com/terms/domain/MaRDI/Section_3/Set_2/Question_4'],
+         ['http://example.com/terms/domain/MaRDI/Section_3/Set_3/Question_1','http://example.com/terms/domain/MaRDI/Section_3/Set_3/Question_2','http://example.com/terms/domain/MaRDI/Section_3/Set_3/Question_3'],
+         ['http://example.com/terms/domain/MaRDI/Section_4/Set_1/Question_1','http://example.com/terms/domain/MaRDI/Section_4/Set_1/Question_2','http://example.com/terms/domain/MaRDI/Section_4/Set_1/Question_3',
+          'http://example.com/terms/domain/MaRDI/Section_4/Set_1/Question_4','http://example.com/terms/domain/MaRDI/Section_4/Set_1/Question_5','http://example.com/terms/domain/MaRDI/Section_4/Set_1/Question_6',
+          'http://example.com/terms/domain/MaRDI/Section_4/Set_1/Question_7','http://example.com/terms/domain/MaRDI/Section_4/Set_1/Question_8'],
+         ['http://example.com/terms/domain/MaRDI/Section_4/Set_2/Question_1','http://example.com/terms/domain/MaRDI/Section_4/Set_2/Question_2','http://example.com/terms/domain/MaRDI/Section_4/Set_2/Question_3',
+          'http://example.com/terms/domain/MaRDI/Section_4/Set_2/Question_4','http://example.com/terms/domain/MaRDI/Section_4/Set_2/Question_5'],
+         ['http://example.com/terms/domain/MaRDI/Section_4/Set_3/Question_1','http://example.com/terms/domain/MaRDI/Section_4/Set_3/Question_2','http://example.com/terms/domain/MaRDI/Section_4/Set_3/Question_3',
+          'http://example.com/terms/domain/MaRDI/Section_4/Set_3/Question_4','http://example.com/terms/domain/MaRDI/Section_4/Set_3/Question_5','http://example.com/terms/domain/MaRDI/Section_4/Set_3/Question_6',
+          'http://example.com/terms/domain/MaRDI/Section_4/Set_3/Question_7','http://example.com/terms/domain/MaRDI/Section_4/Set_3/Question_8','http://example.com/terms/domain/MaRDI/Section_4/Set_3/Question_9'],
+         ['http://example.com/terms/domain/MaRDI/Section_4/Set_5/Question_1','http://example.com/terms/domain/MaRDI/Section_4/Set_5/Question_2','http://example.com/terms/domain/MaRDI/Section_4/Set_5/Question_3',
+          'http://example.com/terms/domain/MaRDI/Section_4/Set_5/Question_4','http://example.com/terms/domain/MaRDI/Section_4/Set_5/Question_5','http://example.com/terms/domain/MaRDI/Section_4/Set_5/Question_6',
+          'http://example.com/terms/domain/MaRDI/Section_4/Set_5/Question_7','http://example.com/terms/domain/MaRDI/Section_4/Set_5/Question_8'],
+         ['http://example.com/terms/domain/MaRDI/Section_4/Set_6/Question_0','http://example.com/terms/domain/MaRDI/Section_4/Set_6/Question_1','http://example.com/terms/domain/MaRDI/Section_4/Set_6/Question_2',
+          'http://example.com/terms/domain/MaRDI/Section_4/Set_6/Question_3','http://example.com/terms/domain/MaRDI/Section_4/Set_6/Question_4','http://example.com/terms/domain/MaRDI/Section_4/Set_6/Question_5',
+          'http://example.com/terms/domain/MaRDI/Section_4/Set_6/Question_6','http://example.com/terms/domain/MaRDI/Section_4/Set_6/Question_7','http://example.com/terms/domain/MaRDI/Section_4/Set_6/Question_8',
+          'http://example.com/terms/domain/MaRDI/Section_4/Set_6/Question_9'],
+         ['http://example.com/terms/domain/MaRDI/Section_4/Set_7/Question_0','http://example.com/terms/domain/MaRDI/Section_4/Set_7/Question_1','http://example.com/terms/domain/MaRDI/Section_4/Set_7/Question_2',
+          'http://example.com/terms/domain/MaRDI/Section_4/Set_7/Question_3','http://example.com/terms/domain/MaRDI/Section_4/Set_7/Question_4','http://example.com/terms/domain/MaRDI/Section_4/Set_7/Question_5',
+          'http://example.com/terms/domain/MaRDI/Section_4/Set_7/Question_6','http://example.com/terms/domain/MaRDI/Section_4/Set_7/Question_7','http://example.com/terms/domain/MaRDI/Section_4/Set_7/Question_8',
+          'http://example.com/terms/domain/MaRDI/Section_4/Set_7/Question_9']]
+
+# Stuff for KG export
+
+KG_export=['http://example.com/terms/domain/MaRDI/Section_2/Set_2/Question_1','http://example.com/terms/domain/MaRDI/Section_4/Set_2/Question_2','http://example.com/terms/domain/MaRDI/Section_4/Set_6/Question_1']
+
+# Stuff for Decisions
+
+dec=[[['http://example.com/terms/domain/MaRDI/Section_0/Set_1/Question_1', 'Workflow Documentation'],['http://example.com/terms/domain/MaRDI/Section_0/Set_1/Question_1', 'Workflow Dokumentation']],
+     [['http://example.com/terms/domain/MaRDI/Section_0/Set_1/Question_1', 'Workflow Finding'],['http://example.com/terms/domain/MaRDI/Section_0/Set_1/Question_1', 'Workflow Findung']],
+     [['http://example.com/terms/domain/MaRDI/Section_6/Set_1/Question_1', 'Markdown File'],['http://example.com/terms/domain/MaRDI/Section_6/Set_1/Question_1', 'MaRDI Portal']],
+     [['http://example.com/terms/domain/MaRDI/Section_1/Set_1/Question_0', 'Research Objective'],['http://example.com/terms/domain/MaRDI/Section_1/Set_1/Question_0', 'Forschungsziel']],
+     [['http://example.com/terms/domain/MaRDI/Section_1/Set_1/Question_0', 'Methods'],['http://example.com/terms/domain/MaRDI/Section_1/Set_1/Question_0', 'Methoden']],
+     [['http://example.com/terms/domain/MaRDI/Section_1/Set_1/Question_0', 'Input Data'],['http://example.com/terms/domain/MaRDI/Section_1/Set_1/Question_0', 'Eingabedaten']],
+     [['http://example.com/terms/domain/MaRDI/Section_2/Set_1/Question_3', 'Mathematical Workflow'],['http://example.com/terms/domain/MaRDI/Section_2/Set_1/Question_3', 'Mathematischer Workflow']],
+     [['http://example.com/terms/domain/MaRDI/Section_2/Set_1/Question_3', 'Experimental Workflow'],['http://example.com/terms/domain/MaRDI/Section_2/Set_1/Question_3', 'Experimenteller Workflow']]]
+
+
