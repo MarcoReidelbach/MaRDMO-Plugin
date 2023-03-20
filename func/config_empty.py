1dac37a2efff77ee7db5b404466b5c3778f73bd5
MarcoReidelbach <marcreidel@aol.com>
2023-03-20 16:28:36 +0100

    Add empty config file

---
+++ func/config_empty.py
@@ -0,0 +1,8 @@
+#Portal
+lgname = 
+lgpassword = 
+
+mardi_wiki="http://localhost:8080/wiki/"
+mardi_api="http://localhost:8080/api.php"
+mardi_endpoint="http://localhost:8834/proxy/wdqs/bigdata/namespace/wdq/sparql"
+wikidata_endpoint="https://query.wikidata.org/sparql"
