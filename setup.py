83647076ca3b16d2f5ba5861ea02809cd0a92919
MarcoReidelbach <reidelbach@zib.de>
2024-03-27 12:50:51 +0100

    Consider HTML Templates during pip install...

--- setup.py
+++ setup.py
@@ -25,6 +25,9 @@ setup(
         'Programming Language :: Python :: 3.10'
     ],
     packages=find_packages(),
+    package_data = {'MaRDMO':['templates/MaRDMO/*.html']},
     include_package_data=True,
+
     install_requires=['bibtexparser','langdetect','pylatexenc','pypandoc_binary','wikibaseintegrator']
 )
+
