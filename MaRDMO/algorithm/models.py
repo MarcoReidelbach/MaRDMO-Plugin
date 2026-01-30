b17b3664346d68e1f3d0c2672abd9ae30d528a5f
MarcoReidelbach <reidelbach@zib.de>
2026-01-30 11:01:16 +0100

    removed unnecessary data class fields

--- MaRDMO/algorithm/models.py
+++ MaRDMO/algorithm/models.py
@@ -6,9 +6,7 @@ from ..models import Relatant
 
 @dataclass
 class Benchmark:
-    id: Optional[str]
-    label: Optional[str]
-    description: Optional[str]
+    '''Data Class For Benchmark Item'''
     reference: Optional[List] = field(default_factory=list)
     publications: Optional[List[Relatant]] = field(default_factory=list)
     
@@ -20,18 +18,12 @@ class Benchmark:
         order = add_reference_order('benchmark')
 
         return cls(
-            id = None,
-            label = None,
-            description = None,
             reference = {order[key][0]: [order[key][1], value] for part in data.get('reference', {}).get('value', '').split(' | ') if (key := part.split(':')[0]) in order and (value := part.split(':')[1])} | ({order['url'][0]: [order['url'][1], url]} if (url := next((part for part in data.get('reference', {}).get('value', '').split(' | ') if part.startswith('https://')), None)) else {}),
             publications = [Relatant.from_query(publication) for publication in data.get('publication', {}).get('value', '').split(" / ") if publication] if 'publication' in data else []
         )
         
 @dataclass
 class Software:
-    id: Optional[str]
-    label: Optional[str]
-    description: Optional[str]
     reference: Optional[List] = field(default_factory=list)
     tests: Optional[List[Relatant]] = field(default_factory=list)
     publications: Optional[List[Relatant]] = field(default_factory=list)
@@ -44,9 +36,6 @@ class Software:
         order = add_reference_order('software')
 
         return cls(
-            id = None,
-            label = None,
-            description = None,
             reference = {order[key][0]: [order[key][1], value] for part in data.get('reference', {}).get('value', '').split(' | ') if (key := part.split(':')[0]) in order and (value := part.split(':')[1])} | ({order['url'][0]: [order['url'][1], url]} if (url := next((part for part in data.get('reference', {}).get('value', '').split(' | ') if part.startswith('https://')), None)) else {}),
             tests =  [Relatant.from_query(benchmark) for benchmark in data.get('tests', {}).get('value', '').split(" / ") if benchmark] if 'tests' in data else [], 
             publications = [Relatant.from_query(publication) for publication in data.get('publication', {}).get('value', '').split(" / ") if publication] if 'publication' in data else []
@@ -54,9 +43,6 @@ class Software:
     
 @dataclass
 class Problem:
-    id: Optional[str]
-    label: Optional[str]
-    description: Optional[str]
     specializes: Optional[List[Relatant]] = field(default_factory=list)
     specializedBy: Optional[List[Relatant]] = field(default_factory=list)
     instantiates: Optional[List[Relatant]] = field(default_factory=list)
@@ -68,9 +54,6 @@ class Problem:
         data = raw_data[0]
 
         return cls(
-            id = None,
-            label = None,
-            description = None,
             specializes = [Relatant.from_query(specializes) for specializes in data.get('specializes', {}).get('value', '').split(" / ") if specializes] if 'specializes' in data else [], 
             specializedBy = [Relatant.from_query(specializedBy) for specializedBy in data.get('specializedBy', {}).get('value', '').split(" / ") if specializedBy] if 'specializedBy' in data else [], 
             instantiates =  [Relatant.from_query(benchmark) for benchmark in data.get('instantiates', {}).get('value', '').split(" / ") if benchmark] if 'instantiates' in data else [], 
@@ -78,9 +61,6 @@ class Problem:
     
 @dataclass
 class Algorithm:
-    id: Optional[str]
-    label: Optional[str]
-    description: Optional[str]
     componentOf: Optional[List[Relatant]] = field(default_factory=list)
     hasComponent: Optional[List[Relatant]] = field(default_factory=list)
     subclassOf: Optional[List[Relatant]] = field(default_factory=list)
@@ -97,9 +77,6 @@ class Algorithm:
         data = raw_data[0]
 
         return cls(
-            id = None,
-            label = None,
-            description = None,
             componentOf = [Relatant.from_query(componentOf) for componentOf in data.get('componentOf', {}).get('value', '').split(" / ") if componentOf] if 'componentOf' in data else [], 
             hasComponent = [Relatant.from_query(hasComponent) for hasComponent in data.get('hasComponent', {}).get('value', '').split(" / ") if hasComponent] if 'hasComponent' in data else [], 
             subclassOf = [Relatant.from_query(subclassOf) for subclassOf in data.get('subclassOf', {}).get('value', '').split(" / ") if subclassOf] if 'subclassOf' in data else [], 
