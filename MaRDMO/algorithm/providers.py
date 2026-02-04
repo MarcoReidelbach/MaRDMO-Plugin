10f665c93e5b7a854e8207afa60e0443dfc214c3
MarcoReidelbach <reidelbach@zib.de>
2026-02-04 10:15:07 +0100

    added benchmark/software provider

--- MaRDMO/algorithm/providers.py
+++ MaRDMO/algorithm/providers.py
@@ -145,3 +145,27 @@ class RelatedBenchmarkWithCreation(Provider):
             project = project,
             setup = setup
         )
+    
+class RelatedBenchmarkOrSoftwareWithoutCreation(Provider):
+    '''Benchmark, Software Provider (MaRDI Portal),
+       No User Creation, No Refresh Upon Selection
+    '''
+
+    search = True
+
+    def get_options(self, project, search=None, user=None, site=None):
+        '''Queries external source for user input'''
+        if not search or len(search) < 3:
+            return []
+
+        # Define the query_setup
+        setup = define_setup(
+            query_attributes = ['benchmark', 'software'],
+            sources = ['mardi']
+        )
+
+        return query_sources_with_user_additions(
+            search = search,
+            project = project,
+            setup = setup
+        )
