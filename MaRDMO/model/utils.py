cdd253e170270795e114dda895c5b1470b22b70f
MarcoReidelbach <reidelbach@zib.de>
2026-02-05 20:35:39 +0100

    reject documentations with not found statements, re-ordered functions

--- MaRDMO/model/utils.py
+++ MaRDMO/model/utils.py
@@ -38,3 +38,86 @@ def map_entity_quantity(data, entity_type, mapping):
                 qtype = quantity.get("QorQK")
                 if qtype in mapping.values():
                     element["Info"] = build_quantity_info(quantity, qtype)
+
+def error_message(section, page, message):
+    '''Generate Error Message'''
+    return f"{section} (Page {page}): {message}"
+
+def check_relation_static(data, page_name, error, relation, from_class, to_class):
+    '''Check Components of static Statements'''
+    if not data.get(relation):
+        error.append(
+            error_message(
+                section = from_class,
+                page = page_name,
+                message = f'Missing {to_class}'
+            )
+        )
+    elif 'not found' in data.get(relation, {}).values():
+        error.append(
+            error_message(
+                section = from_class,
+                page = page_name,
+                message = f'Selected {to_class} not found in {to_class} Section'
+            )
+        )
+
+def check_relation_flexible(
+        data,
+        page_name,
+        error,
+        relation,
+        from_class,
+        to_class=None,
+        optional=True
+):
+    '''Check Components of flexible Statements'''
+    # If no section name provided use class name
+    if not to_class:
+        to_class = from_class
+    # Check Expression Connections
+    if not optional:
+        if not data.get(relation):
+            error.append(
+                error_message(
+                    section = from_class,
+                    page = page_name,
+                    message = f'Missing {to_class}'
+                )
+            )
+    # Check Missing Relation Type
+    if any(
+        val['relation'] == 'MISSING RELATION TYPE'
+        for val in data.get(relation, {}).values()
+    ):
+        error.append(
+            error_message(
+                section = from_class,
+                page = page_name,
+                message = f'Missing Relation Type ({to_class})'
+            )
+        )
+    # Check Missing Object Item
+    if any(
+        val['relatant'] == 'MISSING OBJECT ITEM'
+        for val in data.get(relation, {}).values()
+    ):
+        error.append(
+            error_message(
+                section = from_class,
+                page = page_name,
+                message = f'Missing Object Item ({to_class})'
+            )
+        )
+    # Check Not Found Item
+    if any(
+        val['relatant'] == 'not found'
+        for val in data.get(relation, {}).values()
+    ):
+        error.append(
+            error_message(
+                section = from_class,
+                page = page_name,
+                message = f'Selected {to_class} not found in {to_class} Section'
+            )
+        )
