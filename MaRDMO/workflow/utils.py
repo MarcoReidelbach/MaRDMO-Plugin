d4b53377d36f722c0c937591a1394d820c035dd9
MarcoReidelbach <reidelbach@zib.de>
2026-04-02 20:25:00 +0200

    added helper functions for size, references, and archive

--- MaRDMO/workflow/utils.py
+++ MaRDMO/workflow/utils.py
@@ -1,3 +1,5 @@
+from .constants import data_set_reference_ids
+
 def get_discipline(answers):
     ids = []
     md = 0
@@ -16,5 +18,34 @@ def get_discipline(answers):
                     md += 1
                     ids.append(answers['processstep'][key]['discipline'][key2]['ID'])
     return answers
+
+def get_size(data, options):
+    '''Function which extracts the type and size of data'''
+    size_unit = data.get('size_unit', {}).get('value', '')
+    size_value = data.get('size_value', {}).get('value', '')
+    size_record = data.get('size_record', {}).get('value', '')
+    
+    unit = options.get(size_unit) if size_unit else (options['items'] if size_record else '')
+    value = size_value or size_record
+    
+    return [unit, value] if unit and value else []
+
+def get_reference(data, options):
+    result = {}
+
+    for idx, key in enumerate(data_set_reference_ids):
+        if key == 'Yes' or key == 'No':
+            if value := data.get('publish', {}).get('value') == key:
+                result[idx] = [options[key], '']
+        else:
+            if value := data.get(key, {}).get('value'):
+                result[idx] = [options[key], value]
+
+    return result
+
+def get_archive(data, options):
+    archive = options[data['archive']['value']] if data.get('archive', {}).get('value') else ''
+    year = data.get('end_time', {}).get('value', '')[:4]
+    return [archive, year] if archive else []
 
 
